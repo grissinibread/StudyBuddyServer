1af38d6d6de1ba946a2bbc52061b698b08ee9532
Alejandro Navarro <alejandronavarro@Alejandros-MacBook-Pro-4.local>
2025-02-28 10:34:55 -0800

    Server Skeleton

---
+++ src/main/java/com/studybuddyserver/StudyBuddyServerApplication.java
@@ -0,0 +1,13 @@
+package com.studybuddyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBuddyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBuddyServerApplication.class, args);
+    }
+
+}
