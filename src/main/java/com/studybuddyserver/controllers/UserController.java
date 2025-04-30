64a5eed16586bc856e030d5e969f4a28ad545525
Johnny Qi <qi004@csusm.edu>
2025-04-29 19:22:01 -0700

    I inserted changes

--- src/main/java/com/studybuddyserver/controllers/UserController.java
+++ src/main/java/com/studybuddyserver/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
@@ -23,7 +24,7 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long id) {
+    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long id, @RequestBody UserDto userDto) {
         var user = userRepository.findById(id).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
@@ -35,4 +36,26 @@ public class UserController {
     private UserDto toDto(User user) {
         return new UserDto(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail());
     }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        User user = new User();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(toDto(savedUser));
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if(!userRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        userRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
+
