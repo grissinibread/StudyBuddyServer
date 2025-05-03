a5252b9f67077528364306ae6479ef3a2817665a
Alejandro Navarro <alexnavarro57@gmail.com>
2025-05-03 16:03:47 -0700

    More delombification

--- src/main/java/com/studybuddyserver/dtos/UpdateUserRequest.java
+++ src/main/java/com/studybuddyserver/dtos/UpdateUserRequest.java
@@ -1,12 +1,85 @@
 package com.studybuddyserver.dtos;
 
-import lombok.Data;
-import org.bson.types.ObjectId;
-
-@Data
 public class UpdateUserRequest {
     private String id;
     private String firstName;
     private String lastName;
     private String email;
+
+    public UpdateUserRequest() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof UpdateUserRequest)) return false;
+        final UpdateUserRequest other = (UpdateUserRequest) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$firstName = this.getFirstName();
+        final Object other$firstName = other.getFirstName();
+        if (this$firstName == null ? other$firstName != null : !this$firstName.equals(other$firstName)) return false;
+        final Object this$lastName = this.getLastName();
+        final Object other$lastName = other.getLastName();
+        if (this$lastName == null ? other$lastName != null : !this$lastName.equals(other$lastName)) return false;
+        final Object this$email = this.getEmail();
+        final Object other$email = other.getEmail();
+        if (this$email == null ? other$email != null : !this$email.equals(other$email)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof UpdateUserRequest;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        final Object $firstName = this.getFirstName();
+        result = result * PRIME + ($firstName == null ? 43 : $firstName.hashCode());
+        final Object $lastName = this.getLastName();
+        result = result * PRIME + ($lastName == null ? 43 : $lastName.hashCode());
+        final Object $email = this.getEmail();
+        result = result * PRIME + ($email == null ? 43 : $email.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "UpdateUserRequest(id=" + this.getId() + ", firstName=" + this.getFirstName() + ", lastName=" + this.getLastName() + ", email=" + this.getEmail() + ")";
+    }
 }
--- src/main/java/com/studybuddyserver/entities/User.java
+++ src/main/java/com/studybuddyserver/entities/User.java
@@ -2,25 +2,11 @@ package com.studybuddyserver.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Getter
-@Setter
 @Entity
-@Builder
 @Document(collection = "SB_users")
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class User {
     @Id
     @Field("_id")
@@ -49,6 +35,24 @@ public class User {
     @Field("gradYear")
     private Integer gradYr;
 
+    public User(String Id, String firstName, String lastName, String email, String password, Integer age, String major, Integer gradYr) {
+        this.Id = Id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.major = major;
+        this.gradYr = gradYr;
+    }
+
+    public User() {
+    }
+
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
 //    private List<String> interests = new ArrayList<String>();
 //    private String interest1;
 //    private String interest2;
@@ -64,7 +68,8 @@ public class User {
 //        this.major = major; this.gradYr = gradYr;
 //        this.bio = bio;
 //        this.interest1 = interest1; this.interest2 = interest2; this.interest3 = interest3;
-////        this.interests.add(interest1); this.interests.add(interest2); this.interests.add(interest3);
+
+    /// /        this.interests.add(interest1); this.interests.add(interest2); this.interests.add(interest3);
 //        this.email = email; this.password = password;
 //    }
 
@@ -133,6 +138,104 @@ public class User {
 
     public Integer getGradYr() {
         return this.gradYr;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setGradYr(Integer gradYr) {
+        this.gradYr = gradYr;
+    }
+
+    public String toString() {
+        return "User(Id=" + this.getId() + ", firstName=" + this.getFirstName() + ", lastName=" + this.getLastName() + ", email=" + this.getEmail() + ", password=" + this.getPassword() + ", age=" + this.getAge() + ", major=" + this.getMajor() + ", gradYr=" + this.getGradYr() + ")";
+    }
+
+    public static class UserBuilder {
+        private String Id;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
+        private Integer age;
+        private String major;
+        private Integer gradYr;
+
+        UserBuilder() {
+        }
+
+        public UserBuilder Id(String Id) {
+            this.Id = Id;
+            return this;
+        }
+
+        public UserBuilder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder age(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public UserBuilder major(String major) {
+            this.major = major;
+            return this;
+        }
+
+        public UserBuilder gradYr(Integer gradYr) {
+            this.gradYr = gradYr;
+            return this;
+        }
+
+        public User build() {
+            return new User(this.Id, this.firstName, this.lastName, this.email, this.password, this.age, this.major, this.gradYr);
+        }
+
+        public String toString() {
+            return "User.UserBuilder(Id=" + this.Id + ", firstName=" + this.firstName + ", lastName=" + this.lastName + ", email=" + this.email + ", password=" + this.password + ", age=" + this.age + ", major=" + this.major + ", gradYr=" + this.gradYr + ")";
+        }
     }
 
 //    public void editInterest(String interest1, String interest2, String interest3) {
