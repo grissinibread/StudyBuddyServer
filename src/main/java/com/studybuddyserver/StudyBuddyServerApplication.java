c4b0fd3a74b54bcb8a6be171bd88dd5ce3a538bf
adria <varga418@csusm.edu>
2025-05-05 18:21:33 -0700

    facde pattern implemented, alg fixed

--- src/main/java/com/studybuddyserver/StudyBuddyServerApplication.java
+++ src/main/java/com/studybuddyserver/StudyBuddyServerApplication.java
@@ -1,11 +1,17 @@
 package com.studybuddyserver;
 
+import com.studybuddyserver.controllers.UserController;
+import com.studybuddyserver.dtos.UserDto;
 import com.studybuddyserver.entities.User;
+import com.studybuddyserver.matching.Match;
+import com.studybuddyserver.matching.MatchingAlg;
 import com.studybuddyserver.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StudyBuddyServerApplication {
@@ -13,6 +19,8 @@ public class StudyBuddyServerApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StudyBuddyServerApplication.class, args);
         var repository = context.getBean(UserRepository.class);
+        MatchingAlg matchingAlg = context.getBean(MatchingAlg.class);
+        UserController userController = context.getBean(UserController.class);
 
 //        // This is how you create a user.
 //        var user = User.builder()
@@ -23,6 +31,23 @@ public class StudyBuddyServerApplication {
 //                .build();
 //
 //        repository.save(user);
-    }
 
+        Iterable< UserDto> list = userController.getAllUsers();
+        for (UserDto userDto : list) {
+            System.out.println(userDto.getFirstName());
+        }
+        System.out.println("list end\n");
+
+        // get all users
+        List<User> users = repository.findAll();
+        User loggedIn = users.get(2);
+        List<Match> matches = matchingAlg.returnMatches(loggedIn);
+        System.out.println();
+        System.out.println("User to be matched: " + loggedIn);
+        int i = 1;
+        for (Match match : matches) {
+            System.out.println("Match " + i +"- Name: " + match.getName()+ ", Rank: " + match.getRank());
+            i++;
+        }
+    }
 }
--- src/main/java/com/studybuddyserver/controllers/UserController.java
+++ src/main/java/com/studybuddyserver/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.studybuddyserver.controllers;
 import com.studybuddyserver.dtos.RegisterUserRequest;
 import com.studybuddyserver.dtos.UpdateUserRequest;
 import com.studybuddyserver.dtos.UserDto;
+import com.studybuddyserver.facades.DtoFacade;
 import com.studybuddyserver.mappers.UserMapper;
 import com.studybuddyserver.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -10,61 +11,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.stream.Collectors;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/users")
 public class UserController {
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private final DtoFacade dtoFacade;
 
     @GetMapping()
     public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
-                .stream()
-                .map(userMapper::toDto)
-                .toList();
+        return dtoFacade.getAllUsers();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUser(@PathVariable(name = "id") String id) {
-        var user = userRepository.findById(id).orElse(null);
-        if (user == null)
-            return ResponseEntity.notFound().build();
-
-        return ResponseEntity.ok(userMapper.toDto(user));
+        return dtoFacade.getUser(id);
     }
 
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody RegisterUserRequest register, UriComponentsBuilder uriBuilder) {
-        var user = userMapper.toEntity(register);
-        var savedUser = userRepository.save(user);
-
-        System.out.println(savedUser);
-        var userDto = userMapper.toDto(savedUser);
-        var uri = uriBuilder.path("/users/{id}").buildAndExpand(userDto.getId()).toUri();
-
-        return ResponseEntity.created(uri).body(userDto);
+        return dtoFacade.createUser(register, uriBuilder);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<UserDto> updateUser(@PathVariable String id, @RequestBody UpdateUserRequest update) {
-        var user = userRepository.findById(String.valueOf(id)).orElse(null);
-        if(user == null)
-            return ResponseEntity.notFound().build();
-
-        userMapper.updateUser(update, user);
-        userRepository.save(user);
-
-        return ResponseEntity.ok(userMapper.toDto(user));
+        return dtoFacade.updateUser(id, update);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<UserDto> deleteUser(@PathVariable String id) {
-        var user = userRepository.findById(String.valueOf(id)).orElse(null);
-        if(user == null)
-            return ResponseEntity.notFound().build();
-
-        userRepository.delete(user);
-        return ResponseEntity.ok(userMapper.toDto(user));
+        return dtoFacade.deleteUser(id);
     }
 }
